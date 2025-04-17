0374f21287ff20963b992071f608edaa300233a9
hisabellippel <hisabel_lippel@estudante.sesisenai.org.br>
2025-04-17 08:57:25 -0300

    add validação

--- reforço.js
+++ reforço.js
@@ -1,19 +1,50 @@
-function formularioLogin() {
-    const nome = document.getElementById ('nome').ariaValueMax.trim();
-    if (!nome){
+function alertn() {
+    let nome = document.getElementById ('nome').ariaValueMax.trim();
+    if (!nome || isNaN(nome)){
         alert ('Por favor, preencher o nome');
         return;
      }else{
         console.log (nome);
      }
-     
+
+     const sobrenome = document.getElementById ('sobrenome').ariaValueMax.trim();
+    if (!sobrenome){
+        alert ('Por favor, preencher o sobrenome!');
+        return;
+     }else{
+        console.log (sobrenome);
+     }
+   
+     const idade = parseInt (document.getElementById('idade').value,10);
+    if(isNaN(idade) || idade < 18){
+      alert('A idade para completar a compra deve ser maior que 18 anos!');
+      return;
+    }else{
+      console.log(idade);
+    }
+
+    const email = document.getElementById('email').ariaValueMax.trim();
+    if (!email){
+      alert('Por favor, preencher o E-mail!');
+      return;
+    }else{
+      console.log(email);
+    }
     
-     const credencial = document.getElementById('credencial').ariaValueMax.trim();
-     if (!credencial){
-       alert ('Por favor, insira sua credencial');
+     const telefone = parseInt (document.getElementById('telefone').ariaValueMax.trim());
+     if (!telefone || !telefone.lenght == 11 || isNaN){
+       alert ('Por favor, insira seu telefone!');
        return;
     }else{
-       console.log (credencial);
+       console.log (telefone);
+    }
+
+    const endereco = parseInt (document.getElementById('endereco').ariaValueMax.trim());
+     if (!endereco){
+       alert ('Por favor, insira seu endereço!');
+       return;
+    }else{
+       console.log (endereco);
     }
     
     const senha = parseInt(document.getElementById('senha').value.trim);
@@ -21,7 +52,7 @@ function formularioLogin() {
        alert('Por favor, insira uma senha válida.');
        return;
     }else{
-       console.log(senha)
+       console.log(senha);
     }
     
     const OTP = parseInt(document.getElementById('OTP').value, 10);
@@ -29,6 +60,48 @@ function formularioLogin() {
        alert('Por favor, insira um código válido.');
        return;
     }else{
-       console.log(OTP)
+       console.log(OTP);
     }
+    
+    const cpf = document.getElementById('cpf').value.trim();
+    if (!cpf || !cpf.lenght == 11 || isNaN(cpf)){
+      alert('Por favor, insira um CPF válido');
+      return;
+    }else{
+      console.log(cpf);
+    }
+
+    const RG = document.getElementById('RG').value,10;
+    if (!RG || !RG.lenght == 8 || isNaN(RG)){
+      alert('Por favor, insira um RG válido!');
+      return;
+    }else{
+      console.log(RG);
+    }
+
+    const DN = document.getElementById('DN');
+    if(DN.value == ''){
+      alert("Data não preenchida!");
+      return;
+   }else{
+      console.log(DN);
+   }
+
+   const Profissao = document.getElementById('Profissao');
+   if(!Profissao){
+      alert("Insira sua profissão!");
+      return;
+   }else{
+      console.log(Profissao);
+   }
+
+   const CEP = document.getElementById('CEP');
+   if (!CEP || !CEP.lenght == 8 || isNaN(CEP)){
+      alert('Por favor, insira um CEP válido!');
+      return;
+    }else{
+      console.log(CEP);
+    }
+
+    
     }
